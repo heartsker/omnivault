26a8f316e770b9b6f3f21f058e4ac9627022daaf
Daniel Pustotin <heartsker@gmail.com>
2024-12-02 16:42:05 +0300

    chore: update scripts

--- _infra/pull.sh
+++ _infra/pull.sh
@@ -10,23 +10,22 @@ git submodule update --init --recursive
 
 # Pull changes for each submodule
 git submodule foreach --recursive '
-  echo "🔧 Pulling changes for $name..."
-  if [ -d "$toplevel/$path" ]; then
-    branch=$(git -C "$toplevel/$path" rev-parse --abbrev-ref HEAD || echo "")
-    if [ -z "$branch" ]; then
-      branch=$(git config -f "$toplevel/.gitmodules" submodule."$name".branch || echo "main")
-    fi
-    echo "🔄 Branch: $branch"
-    git -C "$toplevel/$path" fetch origin
-    git -C "$toplevel/$path" reset --hard "origin/$branch" || exit 1
-  else
-    echo "⚠️  Skipping submodule $name (missing path: $path)"
-  fi
+	echo "🔧 Pulling changes for $name..."
+	if [ -d "$toplevel/$path" ]; then
+		branch=$(git -C "$toplevel/$path" rev-parse --abbrev-ref HEAD || echo "")
+		if [ -z "$branch" ]; then
+			branch=$(git config -f "$toplevel/.gitmodules" submodule."$name".branch || echo "main")
+		fi
+		git -C "$toplevel/$path" fetch origin
+		git -C "$toplevel/$path" reset --hard "origin/$branch" || exit 1
+	else
+		echo "⚠️  Skipping submodule $name (missing path: $path)"
+	fi
 '
 
 # Pull changes for the main repository
 echo "⬇️ Pulling changes for the main repository..."
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-git pull origin "$current_branch"
+git pull origin $current_branch
 
 echo "✅ Pull complete!"
